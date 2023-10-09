e1ea65f06da0692f2af1eccd663f06900a3e4b2e
HHappy7 <144217729+HHappy7@users.noreply.github.com>
2023-10-09 11:24:17 +0200

    Add files via upload

---
+++ GroupProject-Layout.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Assignment
+
+# here comes info about the assignment
+
+# ## Import libraries
+
+# here come all the imported libraries
+
+# ## Part I - Data import
+
+# Here comes the first part of the assignment. Importing the filtered data. Create a dataframe.
+# Show the data in a table.
+# 
+
+#  
+
+# ## Part II - Data processing
+
+# Peak finding. Check of common peaks between data sets. 
+
+#  
+
+# ## Part III - Data visualisation
+
+# Conclusion. Relate found info from Part II to our RQ. Perhaps with graphs, maps, etc. 
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
